d9fa1a5b5b9b1a6bbca8f56eee3815bf319113bf
SilverPhantom1 <dylaaanhm@gmail.com>
2023-08-17 03:47:48 -0400

    Segunda Pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'segunda-pagina',
     loadChildren: () => import('./segunda-pagina/segunda-pagina.module').then( m => m.SegundaPaginaPageModule)
   },
+  {
+    path: 'dos',
+    loadChildren: () => import('./dos/dos.module').then( m => m.DosPageModule)
+  },
 ];
 
 @NgModule({
